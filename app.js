a754a65fb595041166bbb4769b795d3d4beae796
Lightraine2 <laurencedale1986@gmail.com>
2019-07-31 21:35:45 +0100

    init

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// How middleware works - basically this is done every time a req is handled. You can assign variables here to add to request handling. You can assign authentication info here too. 
+
+// app.use(function (req, res, next) {
+//     console.log(Date.now());
+//     req.name = 'Laurence Dale';
+//     next();
+// });
+
+// Handlebars Middleware
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Index route
+
+app.get('/', (req, res) => {
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
+
+});
+
+// About route
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+const port = 5000;
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
